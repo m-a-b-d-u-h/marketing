c0c8bbdac4161d7f0fcf09e1aa098df96fecf8cb
Arch - Muhammad Abduh <imuhammadabduh@gmail.com>
2026-06-14 23:05:54 +0700

    feat: add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+export default {
+  apps: [
+    {
+      name: "marketing-app",
+      script: "server.js",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+}
